a7bae3f5c162ddd4d08a6e0d1698cf20ada371e8
Hanieh Arabzadeh <hanieharabzadeh@Haniehs-MacBook-Pro.local>
2020-06-25 20:14:09 -0700

    accessing webcam

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import os
 import sys
 from struct import pack
-# import cv2
+import cv2
 
 
 ######################
@@ -56,7 +56,31 @@ def messageWindow(self):
             panel = tkinter.Label(self.root, image = imgLabel) 
             panel.grid(row = i, column=j)
     self.root.mainloop()
+def access_to_web_cam():
+    cap = cv2.VideoCapture(0)
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
 
+    while True:
+        ret, frame = cap.read()
+        img_counter = 0
+        try:
+            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            cv2.imshow('Input', frame)
+
+            c = cv2.waitKey(1)
+            if c == 27:
+                break
+            elif c == 32:
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                img_counter += 1
+        except:
+            print("no access")
+    cap.release()
+    cv2.destroyAllWindows()
 # string = "Disconnect"
 # encode = string.encode('utf-8')
 # length = pack('10s', encode)
@@ -67,3 +91,4 @@ def messageWindow(self):
 #     h = Home_Page()
 # received_image("1.jpg",0)
     # h.messageWindow()
+access_to_web_cam()

0eeb7c70e88ad0a15774789cc1805d949526937d
ankitd3 <ankit.dalal488@gmail.com>
2020-06-28 00:50:27 -0700

    Changing absolute path to relative path

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import sys
 sys.path.insert(1, './change_background')
 from change_back import driver
 
-processed_img = driver('Empty-Desks.png','/Users/ankitd3/Documents/USC/wePodia/Athena-Virtual-Classroom/change_background/images')
+processed_img = driver('Empty-Desks.png','./change_background/images')
 # The driver returns the image as well as saves it in images directory
 # Path to processed image: "./change_background/images/processed_img.png"

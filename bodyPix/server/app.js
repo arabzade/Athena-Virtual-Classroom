13302b6f1e8a41aa51afcf784d184fa648fd3677
ankitd3 <ankit.dalal488@gmail.com>
2020-07-17 14:00:24 -0700

    body-pix integration

--- bodyPix/server/app.js
+++ bodyPix/server/app.js
@@ -1,36 +1,51 @@
 const tf = require('@tensorflow/tfjs-node');
-const fs = require("fs");
+// const fs = require("fs");
 const bodyPix = require('@tensorflow-models/body-pix');
 const http = require('http');
 (async () => {
     const net = await bodyPix.load({
+        // architecture: 'MobileNetV1',
+        // outputStride: 16,
+        // multiplier: 0.50,
+        // quantBytes: 1,
         architecture: 'MobileNetV1',
         outputStride: 16,
         multiplier: 0.75,
-        quantBytes: 2,
+        quantBytes: 2
     });
-    const server = http.createServer();
+    const server = http.createServer(function(req,res){
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        if ( req.method === 'OPTIONS' ) {
+            res.writeHead(200);
+            res.end();
+            return;
+        }
+    });
+
     server.on('request', async (req, res) => {
+
         var chunks = [];
         req.on('data', (chunk) => {
             chunks.push(chunk);
         });
         req.on('end', async () => {
             const image = tf.node.decodeImage(Buffer.concat(chunks));
-            fs.writeFile("out.png", Buffer.concat(chunks), 'base64', function(err) {
-                console.log("error",err);
-              });
+            // fs.writeFile("out.png", Buffer.concat(chunks), 'base64', function(err) {
+            //     console.log("error",err);
+            //   });
             segmentation = await net.segmentPerson(image, {
                 flipHorizontal: false,
                 internalResolution: 'medium',
                 segmentationThreshold: 0.7,
             });
             // const maskImage = bodyPix.toMask(segmentation,true)
-            // console.log(maskImage)          
             res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-            fs.writeFile("out1.png", Buffer.from(segmentation.data), 'base64', function(err) {
-                console.log("error",err);
-              });
+            // fs.writeFile("out1.png", Buffer.from(segmentation.data), 'base64', function(err) {
+            //     console.log("error",err);
+            //   });
             res.write(Buffer.from(segmentation.data));
             res.end();
             tf.dispose(image);
